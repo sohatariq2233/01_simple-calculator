cb16dec5f8842fa02500c967953c15c67e1cf311
sohatariq2233 <sohatariq2233@gmail.com>
2024-05-25 11:45:32 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";          
+
+// Asking Question from Uses through Inquirer
+
+let answer = await inquirer.prompt([
+    {message: "Enter first Number", type:"number", name: "firstNumber"},
+    {message: "Enter Second Number", type:"number", name: "secondNumber"},
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        }
+]);
+
+// Conditional statement If-Else
+
+if(answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if(answer.operator === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber)
+}
+else if (answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber)
+}
+else if (answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
